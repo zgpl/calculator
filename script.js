c1e2fa015e5aa5d57a06799627f18bdc641e2d86
Zaina <zeinaeabdelfattah@gmail.com>
2025-10-15 18:37:56 +0400

    debugged a bunch of stuff and then added cool stuff i am the best

--- script.js
+++ script.js
@@ -19,15 +19,13 @@ let operator = '';
 let secondNumber = '';
 
 const operate = function (firstNumber, operator, secondNumber) {
-    const expression = `${firstNumber} ${operator} ${secondNumber}`;
-    console.log(expression);
-    if (operator == '+') {
+    if (operator === '+') {
         return add(firstNumber, secondNumber);
-      } else if (operator == '-') {
+      } else if (operator === '-') {
         return subtract(firstNumber, secondNumber);
-       } else if (operator == '*') {
+       } else if (operator === '*') {
         return multiply(firstNumber, secondNumber);
-        } else if (operator == '/') {
+        } else if (operator === '/') {
         return divide(firstNumber, secondNumber);
       } else {
         return null;
@@ -36,23 +34,38 @@ const operate = function (firstNumber, operator, secondNumber) {
 
 const buttons = document.querySelectorAll('.buttons button')
 const numberButtons = document.querySelectorAll('.number');
+const decimalPointButton = document.querySelector('.decimal-point');
 const operatorButtons = document.querySelectorAll('.operator');
-const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
+const clearEntryButton = document.querySelector('.clear-entry');
+const allClearButton = document.querySelector('.all-clear');
 const display = document.querySelector('.display');
 let currentDisplay = display.textContent;
-let readyToAppendNumbers = false;
+
+let replaceDisplayOnNextInput = false;
+let decimalIsBanned = false;
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        if (button.classList.contains('clear')) {
-            display.textContent = "0";
+        if (button.classList.contains('decimal-point')) {
+            if (decimalIsBanned === true) {
+            } else {
+                display.textContent += '.';
+                decimalIsBanned = true;
+            }
+        } else if (button.classList.contains('clear-entry')) {
+            display.textContent = '0';
+            decimalIsBanned = false;
+        } else if (button.classList.contains('all-clear')) {
+            display.textContent = '0';
             firstNumber = '';
             secondNumber = '';
             operator = '';
+            decimalIsBanned = false;
         } else if (button.classList.contains('number')) { 
-            if (display.textContent === "0" || readyToAppendNumbers) {
+            if (display.textContent === '0' || replaceDisplayOnNextInput) {
                 display.textContent = button.textContent;
-                // idk if i need to reset this: readyToAppendNumbers = false;
+                replaceDisplayOnNextInput = false;
             } else {
                 display.textContent += button.textContent;
             } 
@@ -62,20 +75,58 @@ buttons.forEach(button => {
                 secondNumber = Number(display.textContent);
             }
         } else if (button.classList.contains('operator')) {
-            if (button.textContent === "=") {
-            readyToAppendNumbers = false;
-            const result = operate(firstNumber, operator, secondNumber);
-            display.textContent = result;
+            decimalIsBanned = false;
+            if (button.textContent === '=') {
+                if (firstNumber === '' && operator === '' && secondNumber === '') {
+                    display.textContent = '0';
+                } else if (firstNumber !== '' && operator === '' && secondNumber === '') {
+                    display.textContent = firstNumber;
+                } else if (firstNumber !== '' && operator !== '' && secondNumber === '') {
+                    display.textContent = '0';
+                } else {
+                    replaceDisplayOnNextInput = true;
+                    if (secondNumber === 0) {
+                        display.textContent = "naughty!"; 
+                    } else {
+                        const initialResult = operate(firstNumber, operator, secondNumber);
+                        const result = Math.round(initialResult * 100) / 100;
+                        display.textContent = result; 
+                    }
+                }
             } else {
                 if (firstNumber && operator && secondNumber) {
-                    const result = operate(firstNumber, operator, secondNumber);
-                    display.textContent = result; 
-                    firstNumber = result;
-                    secondNumber = '';
+                    let result;
+                    if (secondNumber === 0) {
+                        display.textContent = "naughty!"; 
+                        firstNumber = '';
+                        secondNumber = '';  
+                        operator = '';
+                        replaceDisplayOnNextInput = true;
+                    } else {
+                        const initialResult = operate(firstNumber, operator, secondNumber);
+                        const result = Math.round(initialResult * 100) / 100;
+                        display.textContent = result;
+                        firstNumber = result;
+                        secondNumber = ''; 
+                    }
                 }
                     operator = button.textContent;
-                    readyToAppendNumbers = true;
+                    replaceDisplayOnNextInput = true;
                 } 
             } currentDisplay = display.textContent;
     });
 });
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        equalButton.click();
+    } else if (event.key === 'Backspace') {
+        clearEntryButton.click();
+    }  else {
+        buttons.forEach(button => {
+        if (event.key === button.textContent) {
+            button.click();
+    }
+    });
+    }  
+    });
